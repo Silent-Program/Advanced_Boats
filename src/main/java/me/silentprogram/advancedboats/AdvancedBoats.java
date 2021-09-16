e5da97b255cd4f07e459d817cb14f0649eb0dcfa
Silent-Program <72319773+Silent-Program@users.noreply.github.com>
2021-09-16 10:26:35 -0700

    Initial commit

---
+++ src/main/java/me/silentprogram/advancedboats/AdvancedBoats.java
@@ -0,0 +1,17 @@
+package me.silentprogram.advancedboats;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancedBoats extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
