3f48c0a7d7d70796853c3a8c51223f494a19d378
Silent-Program <72319773+Silent-Program@users.noreply.github.com>
2021-09-20 09:57:33 -0700

    Made some broken ass code

--- src/main/java/me/silentprogram/advancedboats/commands/TestCommand.java
+++ src/main/java/me/silentprogram/advancedboats/commands/TestCommand.java
@@ -1,11 +1,13 @@
 package me.silentprogram.advancedboats.commands;
 
 import me.silentprogram.advancedboats.AdvancedBoats;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class TestCommand implements CommandExecutor {
 	AdvancedBoats plugin;
@@ -20,6 +22,7 @@ public class TestCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		Player plr = (Player) sender;
 		ArmorStand as = plr.getLocation().getWorld().spawn(plr.getLocation(), ArmorStand.class);
+		as.getEquipment().setHelmet(new ItemStack(Material.ACACIA_LOG));
 		as.addPassenger(plr);
 		return true;
 	}
--- src/main/java/me/silentprogram/advancedboats/listeners/Listeners.java
+++ src/main/java/me/silentprogram/advancedboats/listeners/Listeners.java
@@ -7,14 +7,14 @@ import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import me.silentprogram.advancedboats.AdvancedBoats;
-import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.util.EulerAngle;
+import org.bukkit.util.NumberConversions;
 import org.bukkit.util.Vector;
 
-import java.util.Random;
 
 public class Listeners {
 	AdvancedBoats plugin;
@@ -30,14 +30,10 @@ public class Listeners {
 			@Override
 			public void onPacketReceiving(PacketEvent event) {
 				Player plr = event.getPlayer();
-				if(plr.getVehicle() == null) return;
+				if (plr.getVehicle() == null) return;
 				ArmorStand entity = (ArmorStand) plr.getVehicle();
 				
-				Location locCopy = entity.getLocation().clone();
-				locCopy.setPitch(plr.getLocation().getPitch());
-				locCopy.setYaw(plr.getLocation().getYaw());
-				
-				entity.teleport(locCopy);
+				entity.setRotation(plr.getLocation().getYaw(), plr.getLocation().getPitch());
 			}
 		});
 		manager.addPacketListener(new PacketAdapter(plugin, ListenerPriority.NORMAL, PacketType.Play.Client.STEER_VEHICLE) {
@@ -45,14 +41,32 @@ public class Listeners {
 			public void onPacketReceiving(PacketEvent event) {
 				Player plr = event.getPlayer();
 				Entity entity = plr.getVehicle();
-				if(entity == null) return;
+				if (entity == null) return;
 				PacketContainer packet = event.getPacket();
 				float sidewaysFloat = packet.getFloat().read(0);
 				float forwardFloat = packet.getFloat().read(1);
-				
-				entity.setVelocity(new Vector((sidewaysFloat == 0 ? 0 : (sidewaysFloat > 0 ? 0.10 : -0.10)), 0, (forwardFloat == 0 ? 0 : (forwardFloat > 0 ? 0.10 : -0.10))));
+				boolean isJump = packet.getBooleans().read(0);
+				if(forwardFloat == 0 && sidewaysFloat == 0 && !isJump) return;
+				Vector vector = generateVector(plr, forwardFloat, sidewaysFloat);
+				double yVel = entity.getVelocity().getY();
+				//for boats check if the location below the player is water, if so set the Y to like 0.1 or smthng small
+				entity.setVelocity(vector.multiply(0.5).setY((isJump ? (entity.isOnGround() ? 0.4 : yVel) : yVel)));
 			}
 		});
 	}
 	
+	private Vector generateVector(Player p, float forwards, float sideways) {
+		Vector rightVector = new Vector(-Math.cos(Math.toRadians(p.getLocation().getYaw())), 0, -Math.sin(Math.toRadians(p.getLocation().getYaw())));
+		
+		Vector velocity = p.getLocation().getDirection();
+		velocity.setY(0);
+		velocity.normalize();
+		velocity.multiply(forwards);
+		velocity.add(rightVector.multiply(-sideways));
+		velocity.normalize();
+		if (!NumberConversions.isFinite(velocity.getX())) velocity.setX(0);
+		if (!NumberConversions.isFinite(velocity.getY())) velocity.setY(0);
+		if (!NumberConversions.isFinite(velocity.getZ())) velocity.setZ(0);
+		return velocity;
+	}
 }
