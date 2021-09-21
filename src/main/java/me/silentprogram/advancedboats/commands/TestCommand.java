aec6f50940d6491d2a3927d298072a692adf364b
Silent-Program <72319773+Silent-Program@users.noreply.github.com>
2021-09-20 17:38:11 -0700

    Made some broken ass code

--- src/main/java/me/silentprogram/advancedboats/commands/TestCommand.java
+++ src/main/java/me/silentprogram/advancedboats/commands/TestCommand.java
@@ -8,6 +8,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class TestCommand implements CommandExecutor {
 	AdvancedBoats plugin;
@@ -15,14 +16,19 @@ public class TestCommand implements CommandExecutor {
 	public TestCommand(AdvancedBoats plugin) {
 		this.plugin = plugin;
 		plugin.getCommand("test").setExecutor(this);
-		System.out.println("Registered Command!");
 	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		Player plr = (Player) sender;
 		ArmorStand as = plr.getLocation().getWorld().spawn(plr.getLocation(), ArmorStand.class);
-		as.getEquipment().setHelmet(new ItemStack(Material.ACACIA_LOG));
+		ItemStack item = new ItemStack(Material.STICK);
+		ItemMeta itemMeta = item.getItemMeta();
+		itemMeta.setCustomModelData(123456);
+		item.setItemMeta(itemMeta);
+		as.getEquipment().setHelmet(item);
+		plr.getInventory().addItem(item);
+		as.setVisible(false);
 		as.addPassenger(plr);
 		return true;
 	}
